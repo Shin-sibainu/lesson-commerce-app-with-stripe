83f64996d2adb2678795c4d3ac18efa84bf6fcf5
Shin-sibainu <shincode0712@gmail.com>
2024-04-04 12:59:21 +0900

    done

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import Image from "next/image";
+import { cookies } from "next/headers";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Database } from "@/lib/database.types";
+
+const supabase = createServerComponentClient<Database>({ cookies });
+
+const getAllLessons = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+  return lessons;
+};
+
+export default async function Home() {
+  const lessons = await getAllLessons();
+
+  return (
+    <main className="w-full max-w-3xl mx-auto my-16 px-2">
+      <div className="flex flex-col gap-4">
+        {lessons?.map((lesson) => (
+          <Link href={`/${lesson.id}`} key={lesson.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{lesson.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{lesson.description}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
